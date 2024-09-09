07dc47a2e85455dc12675d0b0a7acb5dde138f28
ahmedabougabal <ahmedabougabal@live.com>
2024-09-10 00:14:05 +0300

    feat: adds flask migrate to app.pu since i have made some changes to the user model

--- Day2_Task2/app.py
+++ Day2_Task2/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, redirect, request, session, f
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
+from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
